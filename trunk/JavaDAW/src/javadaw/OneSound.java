cbf6a2770b6dfb36cfeaf9ba2fb3a94df3f78c50
pioras.lodz@gmail.com <pioras.lodz@gmail.com@ae8a360a-ba37-11de-8be4-e1425cda3908>
2009-10-27 13:20:25 +0000

    pliki z polskimi znakami juz wczytuja sie poprawnie, rozwiazanie samego mistrza: Jouvieje :)

--- trunk/JavaDAW/src/javadaw/OneSound.java
+++ trunk/JavaDAW/src/javadaw/OneSound.java
@@ -12,9 +12,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jouvieje.FmodEx.Channel;
+import org.jouvieje.FmodEx.Defines.FMOD_MODE;
 import org.jouvieje.FmodEx.Enumerations.FMOD_CHANNELINDEX;
 import org.jouvieje.FmodEx.Enumerations.FMOD_RESULT;
 import org.jouvieje.FmodEx.Misc.BufferUtils;
@@ -34,57 +36,26 @@ public class OneSound {
     private Sound sound;
     private FMOD_RESULT result;
     private Channel channel;
-    private String path;
 
     private boolean paused;
 
     public OneSound(JDAWEngine jDAWEngine, String path) {
-        this.jDAWEngine = jDAWEngine;
-        this.sound = new Sound();
-        this.result = this.jDAWEngine.getSystem().createStream(path, FMOD_DEFAULT, null, this.sound);
-        this.path = path;
-        /**if (result == FMOD_RESULT.FMOD_ERR_FILE_NOTFOUND) {
-            File file = new File(path);
-            if (file.exists()) {
-                String tmpPath = path.replace('ę', 'e').replace('ó', 'o').replace('ą', 'a')
-                        .replace('ś', 's').replace('ł', 'l').replace('ż', 'z')
-                        .replace('ź', 'z').replace('ć', 'c').replace('ń', 'n');
-                int lastIndexOfDir = tmpPath.lastIndexOf("/");
-                File file2 = new File(tmpPath);
-                
-                FileInputStream from = null;
-                FileOutputStream to = null;
-                try {
-                    from = new FileInputStream(file);
-                    to = new FileOutputStream(file2);
-                    byte[] buffer = new byte[4096];
-                    int bytesRead;
+            this.jDAWEngine = jDAWEngine;
+            this.sound = new Sound();
+            //System.out.println(java.nio.charset.Charset.defaultCharset().name());
+            //System.out.println(path.getBytes().length);
+            //System.out.println(path.length());
 
-                    while ((bytesRead = from.read(buffer)) != -1) {
-                        to.write(buffer, 0, bytesRead); // write
-                    }
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(OneSound.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(OneSound.class.getName()).log(Level.SEVERE, null, ex);
-                } finally {
-                    if (from != null) {
-                        try {
-                            from.close();
-                        } catch (IOException e) {
-                        }
-                    }
-                    if (to != null) {
-                        try {
-                            to.close();
-                        } catch (IOException e) {
-                        }
-                    }
-                
-                this.result = this.jDAWEngine.getSystem().createStream(file2.getAbsolutePath(), FMOD_DEFAULT, null, this.sound);
-            }
-        }}**/
-        SoundUtils.ErrorCheck(result);
+            byte[] bytes = path.getBytes(Charset.forName("UTF-16LE"));
+            ByteBuffer buffer = BufferUtils.newByteBuffer(bytes.length + 2);
+            buffer.put(bytes);
+            BufferUtils.putNullTerminal(buffer);
+            BufferUtils.putNullTerminal(buffer);
+            buffer.rewind();
+
+            this.result = this.jDAWEngine.getSystem().createStream(buffer, FMOD_MODE.FMOD_UNICODE, null, this.sound);
+            SoundUtils.ErrorCheck(result);
+        
     }
 
     public void play() {
