f3d5207dc4622846dca20eef81a0bddb3a1e1747
stan.kacprzak <stan.kacprzak@ae8a360a-ba37-11de-8be4-e1425cda3908>
2009-12-01 10:42:39 +0000

    Dodana Klasa Vocodera, początek pracy nad wyliczaniem obwiedni (envelope)

---
+++ trunk/JavaDAW/src/javadaw/Vocoder.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javadaw;
+
+/**
+ *
+ * @author Stach
+ */
+public class Vocoder {
+
+    /*
+     * Funkcja zwaraca obwiednię sygnału
+     */
+    public int[] getEnvelope(int[] x){
+        int[] envelope=new int[x.length];
+
+
+        return envelope;
+    }
+
+}
